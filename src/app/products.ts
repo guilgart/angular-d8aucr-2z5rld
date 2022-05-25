f66b9eb0481b1e2b286e3fd8a828cec49db1718d
guilgart <guilgart@gmail.com>
2022-05-25 13:37:18 -0500

    5 files modified

--- src/app/products.ts
+++ src/app/products.ts
@@ -14,27 +14,27 @@ export const products = [
     name: 'Pan de Mija',
     price: 6000,
     description: 'Uno de nuestros panes mas vendidos! para disfruta en familia.',
-    presentacion: 'familiar',
+    presentacion: 'Familiar',
     peso: 200,
-    imagenURL: "../assets/img/pan01.jpg"
+    imagenURL: "https://cdn.colombia.com/gastronomia/2012/08/29/pan-marraqueta-3132.gif"
   },
   {
     id: 2,
     name: 'Pan de Queso',
     price: 6500,
     description: 'Delicioso y tradicional, pan relleno de queso perfecto para compartir al desayuno!',
-    presentacion: 'tajado grande',
+    presentacion: 'Tajado grande',
     peso: 250,
-    imagenURL: "../assets/img/Pan02.jpg"
+    imagenURL: "https://t2.rg.ltmcdn.com/es/posts/2/0/0/pan_de_maiz_7002_orig.jpg"
   },
   {
     id: 3,
     name: 'Pan 7 Granos Integral',
     price: 7500,
     description: 'Para quienes desean aumentar la fibra en sus dietas, este delicioso pan tiene en sus ingredientes al menos 100gr de 7 diferentes granos de cereal, perfecto para quienes necesitan un dieta rica en fibra.',
-    presentacion: 'tajado grande',
+    presentacion: 'Tajado grande',
     peso: 300,
-    imagenURL: "../assets/img/pan01.jpg"
+    imagenURL: "https://elarboldelpan.com/latiendadelarbol/33-large_default/pan-integral-de-trigo-y-centeno-molde-cortado-750-gr.jpg"
   }
 ];
 
