7a90c33239799269ade21d4b364ca1cfe99c8297
guilgart <guilgart@gmail.com>
2022-05-26 13:13:31 -0500

    7 files modified

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { CartService } from '../cart.service';
 import { products } from '../products';
 
 @Component({
@@ -7,8 +7,10 @@ import { products } from '../products';
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
+
 export class ProductListComponent {
   products = products;
+  itemsQ = this.cartService.getItemsQuantity();
 
   share() {
     window.alert('Gracias por compartir nuestros productos!');
@@ -17,6 +19,8 @@ export class ProductListComponent {
   onNotify() {
     window.alert('You will be notified when the product goes on sale');
   }
+
+  constructor (private cartService: CartService){ }
 }
 
 
