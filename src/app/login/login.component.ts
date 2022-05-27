605c7d63f6422a77fc42cd8369117aba1bb0e948
guilgart <guilgart@gmail.com>
2022-05-26 23:05:31 -0500

    3 files modified, 1 added

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { Router, ActivatedRoute, NavigationExtras } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm = this.formBuilder.group({
+    usuario: '',
+    password: ''
+  });
+
+  
+  constructor(private formBuilder: FormBuilder, private route: ActivatedRoute,  private router: Router, private http: HttpClient) { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(): void {
+    // Process checkout data here
+    const datosUsuario = this.http.get<{dataUsuario: string, dataPassword: string}[]>('/assets/shipping.json');
+    
+    datosUsuario.pipe(take(1)).subscribe((users: any) => {
+      this.datosUsuario = users;
+    });
+
+
+
+
+    if (this.loginForm.get('usuario')?.value == 'admin' && this.loginForm.get('password')?.value == 'pass') {
+      window.alert( this.loginForm.get('usuario')?.value + ' Bienvenido de nuevo a nuestra tienda!' );
+      // window.URL('/product-list')
+      // this.router.navigate(['/app-product-list']);
+      this.router.navigate(['../productList'], { relativeTo: this.route });
+    } else
+    {
+      window.alert('Error!, Usuario o password incorrectos!.' );
+    }
+    
+    // window.alert('Your order has been submitted');
+
+    this.loginForm.reset();
   }
 
 }
